806ea578978e96aa8e53789f4fde64f724a26581
Catherine Lee <lee.catherine77@gmail.com>
2018-04-09 20:36:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
+function logShout(string) {
+  var shout = string;
+  if(shout.toUpperCase() === string) {
+    console.log(string)
+  } else {
+    shout = string.toUpperCase()
+    console.log(shout)
+  }
+}
