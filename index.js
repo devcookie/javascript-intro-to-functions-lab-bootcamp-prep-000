28ef5a1fb0ec7c421068925aa86ed5f1a120873d
Catherine Lee <lee.catherine77@gmail.com>
2018-04-09 20:42:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,3 +26,14 @@ function logWhisper(string) {
   }
 }
 
+function sayHiToGrandma(string) {
+  if(string.toLowerCase() === string) {
+    return "I can't hear you!"
+  } else if(string.toUpperCase() === string) {
+    return "YES INDEED!"
+  } else if(string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+    
+  }
+}
